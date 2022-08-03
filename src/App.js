84ed4668172d6870481237b7bf1049e2e0f58446
lakshmankashyap <lkashyap92@gmail.com>
2022-08-03 13:54:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { myContext, themes, userContext } from "./contexts/myContext";
+import React, { useState, useMemo } from "react";
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState(themes.light);
+  const [userName, setUserName] = useState("kirill");
+  const value = useMemo(() => ({ theme, setTheme }), [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <myContext.Provider value={value}>
+        <userContext.Provider value={userName}>
+          <Header />
+          <div>
+            <h1>Hello World!</h1>
+          </div>
+          <Footer />
+        </userContext.Provider>
+      </myContext.Provider>
+    </>
+  );
+};
+
+const Header = () => {
+  const { theme, setTheme } = React.useContext(myContext);
+  const toggleTheme = () => {
+    setTheme(theme === themes.dark ? themes.light : themes.dark);
+  };
+  return (
+    <>
+      <div
+        style={{
+          display: "inline-block",
+          width: "100%",
+          backgroundColor: theme.background,
+          color: theme.color,
+        }}
+      >
+        <ul style={{ float: "left" }}>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>
+            <button onClick={toggleTheme}>Change Theme</button>
+          </li>
+        </ul>
+        <UserMenu />
+      </div>
+    </>
+  );
+};
+
+const Footer = () => {
+  const { theme } = React.useContext(myContext);
+  return (
+    <div style={{ backgroundColor: theme.background, color: theme.color }}>
+      <p>CopyRight @2022</p>
     </div>
   );
-}
+};
+
+const UserMenu = () => {
+  const { theme } = React.useContext(myContext);
+  const { name } = React.useContext(userContext);
+  return (
+    <div style={{ float: "right" }}>
+      <span>{name}</span>
+      <ul
+        style={{
+          backgroundColor: theme.background,
+          color: theme.color,
+        }}
+      >
+        <li>Profile</li>
+        <li>Settings</li>
+        <li>Log out</li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;
